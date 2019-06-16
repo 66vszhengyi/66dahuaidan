2fc9e9902aa74e08c30882102f551500f8160f1e
Feng Jiang <fjiang4@sheffield.ac.uk>
2019-06-16 21:51:58 +0100

    函数“int main(void)”已有主体

--- 66dahuaidan.cpp
+++ 66dahuaidan.cpp
@@ -5,8 +5,8 @@
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
-	std::cout << "Zhengyi shige Laopifu";
+    std::cout << "Zhengyi shige Laopifu!!!\n"; 
+	int sum = 0, val = 1;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
@@ -19,8 +19,3 @@ int main()
 //   4. 使用错误列表窗口查看错误
 //   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
 //   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
-#include <iostream>
-int main()
-{
-	int sum = 0, val = 1;
-}
